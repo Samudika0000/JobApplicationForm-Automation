0112c014311dd2e24474999eb76a33948b463eb2
Samudika0000 <aruni.samu1@gmail.com>
2025-02-12 17:44:07 +0530

    Fix: date of birth selection in form automation

--- JobApplicationFormTest.java
+++ JobApplicationFormTest.java
@@ -5,13 +5,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+    import javax.swing.*;
+import java.util.Date;
+
 public class JobApplicationFormTest {
     static WebDriver driver;
 
     public JobApplicationFormTest() {
     }
 
-    public void launchBrowser() {
+    public void launchBrowser()  {
         //launchBrowser
         System.setProperty("webdriver.chrome.driver", "/Users/samudikathilakarathne/Downloads/chromedriver-mac-arm64/chromedriver");
         driver = new ChromeDriver();
@@ -20,12 +23,14 @@ public class JobApplicationFormTest {
         driver.get("https://demoqa.com/automation-practice-form");
     }
 
-    public void validFormSubmission() {
+    public void validFormSubmission()  {
 
         driver.get("https://demoqa.com/automation-practice-form");
+
         //Enter first and last name
         driver.findElement(By.id("firstName")).sendKeys("John");
         driver.findElement(By.id("lastName")).sendKeys("Doe");
+
         driver.findElement(By.id("userEmail")).sendKeys("john.doe@example.com");
 
         //gender selection using radio button click
@@ -39,34 +44,44 @@ public class JobApplicationFormTest {
 
         //Enter Contact Number
         driver.findElement(By.id("userNumber")).sendKeys("1234567890");
+
         //date of birth
         driver.findElement(By.id("dateOfBirthInput")).click();
+
         Select month = new Select(driver.findElement(By.className("react-datepicker__month-select")));
         month.selectByVisibleText("January");  // Select january
 
         Select year = new Select(driver.findElement(By.className("react-datepicker__year-select")));
-        year.selectByVisibleText("2025");  // Select 2025
-        /*driver.findElement(By.xpath("//*[@id=\"dateOfBirth\"]/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/div[1]/select")).click();
-        driver.findElement(By.xpath("//*[@id=\"dateOfBirth\"]/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/div[2]/select")).click();
-        driver.findElement(By.xpath("//*[@id=\"dateOfBirth\"]/div[2]/div[2]/div/div/div[2]/div[2]/div[3]/div[3]")).click();*/
+        year.selectByVisibleText("1995");  // Select 1995
+
+        driver.findElement(By.className("react-datepicker__day react-datepicker__day--002")).click();// Select 2
+
         // Enter Subject (Maths) and select it
-        WebElement subjectField = driver.findElement(By.id("subjectsInput"));
+        WebElement subjectField = driver.findElement(By.xpath("subjectsInput"));
         subjectField.sendKeys("Maths");
         subjectField.submit(); // Select from autocomplete
 
-        //Select hobby
-        driver.findElement(By.xpath("//*[@id=\"hobbiesWrapper\"]/div[2]/div[1]/label")).click();
+        /*//Select hobby
+        WebElement CheckBox = driver.findElement(By.xpath("//*[@id=\"hobbiesWrapper\"]/div[2]/div[1]/label")); //Sports
+        CheckBox.click();
+        Thread.sleep(5000);
+        CheckBox.click();
+
+        System.out.println(CheckBox.getText());*/
+
+
+
         //Upload picture
         driver.findElement(By.id("uploadPicture")).sendKeys("/Users/samudikathilakarathne/Documents/img/rev1.jpg");
        // Enter address
         driver.findElement(By.id("currentAddress")).sendKeys("123 Main Street, Anytown, CA 12345");
         //Select State and City
         driver.findElement(By.xpath("//*[@id=\"state\"]/div/div[1]/div[1]")).click();
-        /*driver.findElement(By.id("stateCity-wrapper")).sendKeys("Delhi");*/
+        driver.findElement(By.id("stateCity-wrapper")).sendKeys("Delhi");
         driver.findElement(By.id("submit")).click();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)   {
         JobApplicationFormTest obj = new JobApplicationFormTest();
         obj.launchBrowser();
         obj.validFormSubmission();
@@ -76,5 +91,3 @@ public class JobApplicationFormTest {
     }
 }
 
-//*[@id="genterWrapper"]/div[2]/div[1]/label
-//*[@id="genterWrapper"]/div[2]/div[1]/label
